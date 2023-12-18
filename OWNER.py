c821e6b50bacc908728ab94dcf200b330d46f63d
sarahassang <145998215+sarahassang@users.noreply.github.com>
2023-12-18 23:27:01 +0100

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["JKI_ll"]
+OWNER_NAME = "ibra"
+BOT_TOKEN = "6767424516:AAHvUef6AjSeWNKVPAHdQkT3OvLTMCjqYmY"
+DATABASE = "mongodb+srv://ibrahimo:148200828@cluster0.javk6b0.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/zombieyy"
+GROUP = "https://t.me/zombie2"
+VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
+LOGS = "zombie2"
+
